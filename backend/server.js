8ae0893a33d01b8f2804f3ca94cdde364fc24527
GOLDENSTEIN <morgane.goldenstein-ext@kering.com>
2023-08-25 15:15:49 +0200

    Add morgan in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ dotenv.config( { path: './config/config.env'});
 
 const app = express();
 
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 //Mount routers
 app.use('/api/v1/roofs', roofs);
 
