23e812d9868e65334d2a4c63f49355c9b01e6d72
GOLDENSTEIN <morgane.goldenstein-ext@kering.com>
2023-08-28 17:16:10 +0200

    Manage error by creating errorHandler

--- backend/controllers/roofs.js
+++ backend/controllers/roofs.js
@@ -1,91 +1,75 @@
-const Roof = require('../models/Roof')
+const Roof = require('../models/Roof');
+const asyncHandler = require('../middleware/async')
+const ErrorResponse = require('../utils/errorResponse')
 
 //@desc     Get all roofs
 //@route    GET /api/v1/roofs
 //@access   Public
 
-exports.getRoofs = async (req, res, next) => {
-    try {
+exports.getRoofs = asyncHandler(async (req, res, next) => {
         const roofs = await Roof.find();
 
-        res.status(200).json({success:true, count: roofs.length, data: roofs});
-        
-    } catch (error) {
-        res.status(400).json({success:false, err: error})
-    }
-}
+        res
+        .status(200)
+        .json({success:true, count: roofs.length, data: roofs});
+});
 
 //@desc     Get single roof
 //@route    GET /api/v1/roofs/:id
 //@access   Public
 
-exports.getRoof = async (req, res, next) => {
-    try {
+exports.getRoof = asyncHandler(async (req, res, next) => {
         const roof = await Roof.findById(req.params.id);
 
         if(!roof) {
-            return res.status(400).json({success: false })
+            return next(
+                new ErrorResponse(`Roof not found with id of ${req.params.id}, 404`)
+            );
         }
-
         res.status(200).json({success:true, data: roof});
-    } catch (error) {
-        res.status(400).json({success:false, err: error});
-        // next(error);
-    }
-}
+});
 
 //@desc     Create a roof
 //@route    GET /api/v1/roofs/
 //@access   Private
 
-exports.createRoof = async (req, res, next) => {
-    try {
+exports.createRoof = asyncHandler(async (req, res, next) => {
         const roof = await Roof.create(req.body);
         res.status(201).json({
             success: true,
             data: roof
         })
-    } catch (error) {
-        res.status(400).json({success:false, err: error });
-    }
-
-}
+});
 
 //@desc     Update a roof
 //@route    GET /api/v1/roofs/:id
 //@access   Private
 
-exports.updateRoof = async (req, res, next) => {
-    try {
+exports.updateRoof = asyncHandler(async (req, res, next) => {
         const roof = await Roof.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
         });
     
         if(!roof) {
-            return res.status(400).json({success: false});
+            return next(
+                new ErrorResponse(`Roof not found with id of ${req.params.id}, 404`)
+            );
         }  
         res.status(200).json({success: true, data: roof});
-
-    } catch (error) {
-        res.status(400).json({success: false, err: error});
-    }
-}
+});
 
 //@desc     Delete a roof
 //@route    GET /api/v1/roofs/:id
 //@access   Private
 
-exports.deleteRoof = async (req, res, next) => {
-    try {
+exports.deleteRoof = asyncHandler(async (req, res, next) => {
         const roof = await Roof.findByIdAndDelete(req.params.id);
     
         if(!roof) {
-            return res.status(400).json({success: false});
+            return next(
+                new ErrorResponse(`Roof not found with id of ${req.params.id}, 404`)
+            );
         }  
         res.status(200).json({success: true, data: {}});
-
-    } catch (error) {
-        res.status(400).json({success: false, err: error});
-    }
-}
+});
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const errorHandler = require('./middleware/error')
 const connectDB = require('./config/db');
 
 //Load env vars
@@ -24,6 +25,8 @@ if(process.env.NODE_ENV === 'development') {
 
 //Mount routers
 app.use('/api/v1/roofs', roofs);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
